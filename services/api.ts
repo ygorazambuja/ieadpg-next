29b869d4d6018c4b4a2265bd9e49e35b69b580dc
Ygor Azambuja <ygorazambuja@gmail.com>
2022-03-24 02:39:06 -0400

    chore: update baseURL

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://ieadpg-next.vercel.app";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL,
 });
