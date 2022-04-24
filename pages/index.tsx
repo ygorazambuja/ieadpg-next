ef81bdfc78c55ec71824d908168ae5feeff68392
Ygor Azambuja <ygorazambuja@gmail.com>
2022-04-24 00:29:37 -0400

    feat: remove stats, e adiciona contador de membros

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Heading, SimpleGrid } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
@@ -5,13 +6,55 @@ import { Stat } from "../components/Stat";
 import { TemplateDashboard } from "../components/TemplateDashboard";
 import { useSupabaseAuth } from "../hooks/useSupabaseAuth";
 
-const stats = [
-  { label: "Membros Cadastrados", value: "71,887" },
-  { label: "Aniversariantes do Mês", value: "56.87%" },
-  { label: "Avg. Click Rate", value: "12.87%" },
-];
+import { supabase } from "../database/supabaseClient";
 
-export default function Home() {
+export async function getServerSideProps() {
+  const { count } = await supabase
+    .from("members")
+    .select("*", { count: "exact" });
+
+  const memberCount = {
+    label: "Membros Cadastrados",
+    value: count,
+  };
+  const birthdaysInMonth = {
+    label: "Aniversariantes do Mês",
+    value: "56",
+  };
+  const newMembers = {
+    label: "Novos Membros",
+    value: "12",
+  };
+
+  return {
+    props: {
+      memberCount,
+      birthdaysInMonth,
+      newMembers,
+    },
+  };
+}
+
+type HomeProps = {
+  memberCount: {
+    label: string;
+    value: string;
+  };
+  birthdaysInMonth: {
+    label: string;
+    value: string;
+  };
+  newMembers: {
+    label: string;
+    value: string;
+  };
+};
+
+export default function Home({
+  memberCount,
+  birthdaysInMonth,
+  newMembers,
+}: HomeProps) {
   const { isAuthenticated } = useSupabaseAuth();
 
   const { push } = useRouter();
@@ -24,11 +67,26 @@ export default function Home() {
 
   return (
     <TemplateDashboard>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
       <Heading>Home</Heading>
       <SimpleGrid columns={{ base: 1, md: 3 }} gap={{ base: "5", md: "6" }}>
-        {stats.map(({ label, value }) => (
-          <Stat key={label} label={label} value={value} />
-        ))}
+        <Stat
+          key={memberCount.label}
+          label={memberCount.label}
+          value={memberCount.value}
+        />
+        <Stat
+          key={birthdaysInMonth.label}
+          label={birthdaysInMonth.label}
+          value={birthdaysInMonth.value}
+        />
+        <Stat
+          key={newMembers.label}
+          label={newMembers.label}
+          value={newMembers.value}
+        />
       </SimpleGrid>
     </TemplateDashboard>
   );
